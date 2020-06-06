70c272d431090b0dcce6d50032038aa774a6f610
Pawan Kolhe <contact@pawankolhe.com>
2020-06-06 16:20:59 +0530

    add dotenv condition

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // Loads environment variables from .env
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const path = require("path");
 const express = require("express");
